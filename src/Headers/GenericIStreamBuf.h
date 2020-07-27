7805940684d9972bca057193e25d46b26a5e5635
Meera Shah <sam10sha@gmail.com>
2020-07-26 19:24:22 -0700

    add genericistreambuf

---
+++ src/Headers/GenericIStreamBuf.h
@@ -0,0 +1,14 @@
+#ifndef GENERICISTREAMBUF_H
+#define GENERICISTREAMBUF_H
+
+#include <streambuf>
+
+namespace GenericIOStreamTest
+{
+    class GenericIStreamBuf : public std::basic_streambuf<char>
+    {
+        
+    };
+}
+
+#endif // GENERICISTREAMBUF_H
---
+++ src/Sources/GenericIStreamBuf.cpp
@@ -0,0 +1,2 @@
+#include "GenericIStreamBuf.h"
+#include "stdafx.h"
