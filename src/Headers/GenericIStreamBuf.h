6e049c9d643353e276f7be425f970ee9de853873
Meera Shah <sam10sha@gmail.com>
2020-07-26 20:12:28 -0700

    begin implementation of GenericIStreamBuf

--- src/Headers/GenericIStreamBuf.h
+++ src/Headers/GenericIStreamBuf.h
@@ -7,7 +7,83 @@ namespace GenericIOStreamTest
 {
     class GenericIStreamBuf : public std::basic_streambuf<char>
     {
+    // Public structures
+    public:
+        struct DataRetrieveCallback_base
+        {
+        // Public member functions
+        public:
+            virtual void Invoke(char* const Buffer, const size_t MaxLen)
+            {
+                
+            }
+        };
         
+        template<class T>
+        struct DataRetrieveCallback_member : public DataRetrieveCallback_base
+        {
+        // Public construction
+        public:
+            DataRetrieveCallback_member(T* Ref, size_t (T::* Func)(char* const, const size_t)) :
+                MemRef(Ref),
+                MemFunc(Func)
+            {
+                
+            }
+            
+        // Public member functions
+        public:
+            virtual void Invoke(char* const Buffer, const size_t MaxLen)
+            {
+                (MemRef->*MemFunc)(Buffer, MaxLen);
+            }
+            
+        // Private member variables
+        private:
+            T* MemRef;
+            size_t (T::* MemFunc)(char* const Buffer, const size_t MaxLen);
+        };
+        
+        struct DataRetrieveCallback_plain : public DataRetrieveCallback_base
+        {
+        // Public construction
+        public:
+            DataRetrieveCallback_plain(size_t (*FuncRef)(char* const, const size_t)) :
+                Func(FuncRef)
+            {
+                
+            }
+            
+        // Public member functions
+        public:
+            virtual void Invoke(char* const Buffer, const size_t MaxLen)
+            {
+                (*Func)(Buffer, MaxLen);
+            }
+            
+        // Private member variables
+        private:
+            size_t (*Func)(char* const, const size_t);
+        };
+        
+    // Public construction/destruction
+    public:
+        GenericIStreamBuf(const size_t BufferSize);
+        ~GenericIStreamBuf();
+        
+    // Public member functions
+    public:
+        void RegisterOnDataRetrieveCallback(DataRetrieveCallback_base* const DataRetCB);
+        
+    // Protected member functions
+    protected:
+        virtual std::basic_streambuf<char>::int_type underflow();
+        
+    // Private member variables
+    private:
+        const size_t MemLength;
+        char* InternalMem;
+        DataRetrieveCallback_base* DataRetrieveCB;
     };
 }
 
--- src/Sources/GenericIStreamBuf.cpp
+++ src/Sources/GenericIStreamBuf.cpp
@@ -1,2 +1,37 @@
 #include "GenericIStreamBuf.h"
 #include "stdafx.h"
+
+// Public construction/destruction
+GenericIOStreamTest::GenericIStreamBuf::GenericIStreamBuf(const size_t BufferSize) :
+    MemLength(BufferSize),
+    InternalMem(new char[BufferSize]),
+    DataRetrieveCB(nullptr)
+{
+    std::memset(InternalMem, 0, BufferSize);
+    setp(InternalMem, InternalMem, InternalMem + BufferSize);
+    setg(InternalMem, InternalMem, InternalMem);
+}
+GenericIOStreamTest::GenericIStreamBuf::~GenericIStreamBuf()
+{
+    if(InternalMem)
+    {
+        delete[] InternalMem;
+        InternalMem = nullptr;
+    }
+}
+
+// Public member functions
+void GenericIOStreamTest::GenericIStreamBuf::RegisterOnDataRetrieveCallback(DataRetrieveCallback_base* const DataRetCB)
+{
+    DataRetrieveCB = DataRetCB;
+}
+
+// Protected member functions
+std::basic_streambuf<char>::int_type GenericIOStreamTest::GenericIStreamBuf::underflow()
+{
+    if(DataRetrieveCB)
+    {
+        DataRetrieveCB->Invoke(InternalMem, MemLength);
+    }
+    return std::basic_streambuf<char>::underflow();
+}
